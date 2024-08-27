6b9f095dc4168d35233daca8a076aac4f2b2364f
ppipada <ppipada@gmail.com>
2024-08-27 11:20:28 +0530

    cwe based views for analysis. add tokens

--- src/logging/logging.py
+++ src/logging/logging.py
@@ -1,10 +1,16 @@
 import logging
+import os
 import sys
 
 
 def configure_logging():
+    level_str = os.getenv("LOG_LEVEL")
+    if level_str == "debug":
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
     logging.basicConfig(
-        level=logging.DEBUG,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+        level=level,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Set the log format
         stream=sys.stdout  # Set the stream to stdout
     )
